59635eed6dd78951808ac3b6a3706a4687b50359
Nommadj <mainamasha254@gmail.com>
2023-03-21 17:21:58 +0300

    Consider this files, ignore others

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Page-specific scripts */
+
+/* Index page */
+if (document.body.classList.contains('index')) {
+  /* Add scripts for the index page here */
+}
+
+/* About page */
+if (document.body.classList.contains('about')) {
+  /* Add scripts for the about page here */
+}
+
+/* Services page */
+if (document.body.classList.contains('services')) {
+  /* Add scripts for the services page here */
+}
+
+/* Contact page */
+if (document.body.classList.contains('contact')) {
+  /* Add scripts for the contact page here */
+}
